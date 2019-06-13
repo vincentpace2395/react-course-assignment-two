5e1c50f90d2a1cf098d52af0386aeada548e8437
Vincent Pace <vpace@nhpace.net>
2019-06-13 18:10:29 -0500

    Finish project to display characters of string to screen, allow for character deletion on click

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import './App.css';
+import Validation from './Components/Validation';
+import Char from './Components/Char';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  state = {
+    userInput: ''
+  };
 
-    </div>
-  );
+  inputChangeHandler = event => {
+    this.setState({userInput: event.target.value})
+  };
+
+  deleteCharHandler = index => {
+      const text = this.state.userInput.split('');
+      text.splice(index, 1);
+      const updatedText = text.join('');
+      this.setState({userInput: updatedText});
+  };
+
+  render() {
+      const charList = this.state.userInput.split('').map((char, index) => {
+          return <Char
+              character={char}
+              key={index}
+              clicked={() => this.deleteCharHandler(index)}/>
+      });
+
+      return (
+        <div className="App">
+          <input
+              type="text"
+              onChange={this.inputChangeHandler}
+              value={this.state.userInput} />
+          <p>{this.state.userInput}</p>
+            <Validation inputLength={this.state.userInput.length} />
+            {charList}
+        </div>
+      );
+  }
 }
 
 export default App;
